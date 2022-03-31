a438ffad11c1096252a44afde57371ac75e65ed0
benbushell <benjbushell@gmail.com>
2022-03-31 20:31:45 +0100

    Folder Restructure

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import "./styles.css";
 
 import Header from "./page/Header";
 import Footer from "./page/Footer";
-import ImportSC0 from "./page/mainContent/importSC0";
+import ImportSC0 from "./page/mainContent/importSc0/importSC0";
 import ImportStructures from "./page/mainContent/importStructures";
-import EditFiles from "./page/mainContent/editFiles";
+import EditFiles from "./page/mainContent/editFiles/editFiles";
 
 export default function App() {
   const [data, setData] = React.useState([]);
   const [selected, setSelected] = React.useState([]);
   const [structures, setStructures] = React.useState([]);
+  const [selectionMode, setSelectionMode] = React.useState('single');
 
   return (
     <div className="mainContainer">
---
+++ src/page/mainContent/importSc0/SelectionToolbar.js
@@ -0,0 +1,13 @@
+//import React from "react";
+
+const SeletionToolbar = () =>{
+    return(
+        <div className="toolbar">
+            <div className="">a</div>    
+            <div className="">b</div>    
+            <div className="">c</div>    
+        </div>
+    )
+}
+
+export default SeletionToolbar
